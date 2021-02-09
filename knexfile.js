a561489521c105e82bc393301b477735e7ceadad
Zach Young <zachyoung@Zachs-iMac.local>
2021-02-09 11:21:39 -0600

    knexfile pg changes

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,12 @@
 module.exports = {
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host     : process.env.RDS_HOSTNAME,
+      user     : process.env.RDS_USERNAME,
+      password : process.env.RDS_PASSWORD,
+      port     : process.env.RDS_PORT
+    },
     pool: {
       min: 2,
       max: 10
